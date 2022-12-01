252bf4d46b1ba63ac0a3157ac52603e4a2ca242d
Vadym Baranov <vadym.baranov@hotmail.com>
2022-12-01 11:13:03 +0200

    Table without users created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import productsFromServer from './api/products';
-// import categoriesFromServer from './api/categories';
+import usersFromServer from './api/users';
+import productsFromServer from './api/products';
+import categoriesFromServer from './api/categories';
+
+import { User } from './types/User';
+import { Product } from './types/Product';
+import { Category } from './types/Category';
+
+// enum SortType {
+//   NONE,
+//   ALPHABET,
+//   LENGTH,
+// }
+
+// type ReorderOptions = {
+//   sortType: SortType,
+//   isReversed: boolean,
+// };
+
+export function getUserById(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const categories: Category[] = categoriesFromServer.map(category => ({
+  ...category,
+  user: getUserById(category.ownerId),
+}));
+
+export function getCategoryById(categoryId: number): Category | null {
+  const foundCategory = categoriesFromServer
+    .find(category => category.id === categoryId);
+
+  return foundCategory || null;
+}
+
+export const products: Product[] = productsFromServer.map(product => ({
+  ...product,
+  category: getCategoryById(product.categoryId),
+}));
+
+export function getVisibleProducts(
+  query: string,
+) {
+  const normalizedQuery = query.toLocaleLowerCase();
+
+  return products.filter(product => (
+    product.name.toLocaleLowerCase().includes(normalizedQuery)
+    || product.name.toLocaleLowerCase().includes(normalizedQuery)));
+}
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  // const visibleProducts = getVisibleProducts(productsFromServer, query);
+
   return (
     <div className="section">
       <div className="container">
@@ -23,27 +75,14 @@ export const App: React.FC = () => {
                 All
               </a>
 
-              <a
-                data-cy="FilterUser"
-                href="#/"
-              >
-                User 1
-              </a>
-
-              <a
-                data-cy="FilterUser"
-                href="#/"
-                className="is-active"
-              >
-                User 2
-              </a>
-
-              <a
-                data-cy="FilterUser"
-                href="#/"
-              >
-                User 3
-              </a>
+              {usersFromServer.map(user => (
+                <a
+                  data-cy="FilterUser"
+                  href="#/"
+                >
+                  {user.name}
+                </a>
+              ))}
             </p>
 
             <div className="panel-block">
@@ -53,7 +92,8 @@ export const App: React.FC = () => {
                   type="text"
                   className="input"
                   placeholder="Search"
-                  value="qwe"
+                  value={query}
+                  onChange={((event) => setQuery(event.target.value))}
                 />
 
                 <span className="icon is-left">
@@ -79,37 +119,15 @@ export const App: React.FC = () => {
               >
                 All
               </a>
-
-              <a
-                data-cy="Category"
-                className="button mr-2 my-1 is-info"
-                href="#/"
-              >
-                Category 1
-              </a>
-
-              <a
-                data-cy="Category"
-                className="button mr-2 my-1"
-                href="#/"
-              >
-                Category 2
-              </a>
-
-              <a
-                data-cy="Category"
-                className="button mr-2 my-1 is-info"
-                href="#/"
-              >
-                Category 3
-              </a>
-              <a
-                data-cy="Category"
-                className="button mr-2 my-1"
-                href="#/"
-              >
-                Category 4
-              </a>
+              {categoriesFromServer.map(category => (
+                <a
+                  data-cy="Category"
+                  className="button mr-2 my-1 is-info"
+                  href="#/"
+                >
+                  {category.title}
+                </a>
+              ))}
             </div>
 
             <div className="panel-block">
@@ -187,53 +205,36 @@ export const App: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr data-cy="Product">
-                <td className="has-text-weight-bold" data-cy="ProductId">
-                  1
-                </td>
-
-                <td data-cy="ProductName">Milk</td>
-                <td data-cy="ProductCategory">🍺 - Drinks</td>
-
-                <td
-                  data-cy="ProductUser"
-                  className="has-text-link"
-                >
-                  Max
-                </td>
-              </tr>
-
-              <tr data-cy="Product">
-                <td className="has-text-weight-bold" data-cy="ProductId">
-                  2
-                </td>
-
-                <td data-cy="ProductName">Bread</td>
-                <td data-cy="ProductCategory">🍞 - Grocery</td>
-
-                <td
-                  data-cy="ProductUser"
-                  className="has-text-danger"
-                >
-                  Anna
-                </td>
-              </tr>
-
-              <tr data-cy="Product">
-                <td className="has-text-weight-bold" data-cy="ProductId">
-                  3
-                </td>
-
-                <td data-cy="ProductName">iPhone</td>
-                <td data-cy="ProductCategory">💻 - Electronics</td>
-
-                <td
-                  data-cy="ProductUser"
-                  className="has-text-link"
-                >
-                  Roma
-                </td>
-              </tr>
+              {products.map(product => (
+                <tr data-cy="Product" key={product.id}>
+                  <td
+                    className="has-text-weight-bold"
+                    data-cy="ProductId"
+                    key={product.id}
+                  >
+                    {product.id}
+                  </td>
+                  <td data-cy="ProductName">
+                    {product.name}
+                  </td>
+                  <td data-cy="ProductCategory">
+                    {product.category?.icon}
+                    -
+                    {product.category?.title}
+                  </td>
+                  <td
+                    data-cy="ProductUser"
+                    className={classNames({
+                      'has-text-link': product.category?.user?.sex === 'm',
+                    },
+                    {
+                      'has-text-danger': product.category?.user?.sex === 'f',
+                    })}
+                  >
+                    {product.category?.user?.name}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
